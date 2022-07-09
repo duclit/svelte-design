08ad9d0193e08d1c10168564967178ff5eedd315
Aarav Sareen <96787824+duclit@users.noreply.github.com>
2022-07-08 22:51:00 -0700

    Add files via upload

---
+++ src/Blanket.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let clickHandler: () => void = () => {};
+</script>
+
+<main on:click|self={clickHandler}>
+    <slot></slot>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100vw;
+        height: 100vh;
+
+        background: rgba(0, 0, 0, 0.1);
+    }
+</style>
