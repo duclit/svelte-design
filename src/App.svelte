d444b01f2c094e5483fa5dd3e0b32636183c4bd5
Aarav Sareen <96787824+duclit@users.noreply.github.com>
2022-07-08 22:50:33 -0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,11 +9,18 @@
 	import Range from './Range.svelte';
 	import Textfield from './Textfield.svelte';
 	import Tabs from './Tabs.svelte';
+	import Blanket from './Blanket.svelte';
 
+	import View1 from './Test/view1.svelte';
+import Settings from './Test/Settings.svelte';
+
+	let showPopup = false;
 	let value = '911';
+
+	$: showPopup, document.body.style.overflow = showPopup ? 'hidden' : 'visible' 
 </script>
 
-<!-- Window -->
+<!-- Panel 1: Login -->
 <Flexbox width="100vw" height="100vh" align={['center', 'center']}>
 	<!-- Dialog -->
 	<Flexbox gap="lg" width="300px" height="fit-content" align={['center', 'center']}>
@@ -22,16 +29,22 @@
 		<Range width="100%" title="Phone number" instructions="{value}" bind:value={value} min={0} max={9999999999}/>
 		<Flexbox width="100%" align={['end', 'end']} gap="sm" direction="row">
 			<Button>Cancel</Button>
-			<Button appearance="primary">Login</Button>
+			<Button appearance="primary" clickHandler={() => {showPopup = true}}>Show Popup</Button>
 		</Flexbox>
 	</Flexbox>
-	<Banner title="Delete this post">
-		<BannerDescription>
-			This will delete post <Code>h432Kkl23Jhe3</Code> and all replies permanently.
-		</BannerDescription>
-		<BannerActions>
-			<Button appearance="link-secondary" compact>Cancel</Button>
-			<Button appearance="danger">Delete</Button>
-		</BannerActions>
-	</Banner>
+
+	<!-- Popup -->
+	{#if showPopup}
+		<Blanket clickHandler={() => {showPopup = false}}>
+			<Banner title="Delete this post">
+				<BannerDescription>
+					This will delete post <Code>h432Kkl23Jhe3</Code> and all replies permanently.
+				</BannerDescription>
+				<BannerActions>
+					<Button appearance="link-secondary" compact clickHandler={() => {showPopup = false}}>Cancel</Button>
+					<Button appearance="danger" clickHandler={() => {showPopup = false}}>Delete</Button>
+				</BannerActions>
+			</Banner>
+		</Blanket>
+	{/if}
 </Flexbox>
